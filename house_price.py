effc5bf208afd990e9936296813179b1fa588679
Roy Kim <roy.kim@modelop.com>
2021-06-02 09:12:27 -0600

    add train section
    used for testing smart comment #modelop.train vs #modelop.training

--- house_price.py
+++ house_price.py
@@ -2,6 +2,8 @@ import pandas
 import pickle
 import numpy
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.linear_model import LassoCV
+from sklearn.preprocessing import StandardScaler
 
 
 # modelop.init
@@ -16,6 +18,95 @@ def begin():
     standard_scaler = pickle.load(open("standard_scaler.pickle", "rb"))
     # load train_encoded_columns
     train_encoded_columns = pickle.load(open("train_encoded_columns.pickle", "rb"))
+
+
+# modelop.train
+def train(data):
+    # load data
+    df = pandas.DataFrame(data)
+
+    # set aside ground truth to later re-append to dataframe
+    y_train = df["SalePrice"]
+
+    # dictionaries to convert values in certain columns
+    generic = {"Ex": 4, "Gd": 3, "TA": 2, "Fa": 1, "None": 0}
+    fireplace_quality = {"Ex": 5, "Gd": 4, "TA": 3, "Fa": 2, "Po": 1, "None": 0}
+    garage_finish = {"Fin": 3, "RFn": 2, "Unf": 1, "None": 0}
+
+    # imputations
+    df.loc[:, "GarageYrBlt"] = df.loc[:, "GarageYrBlt"].fillna(df["YearBuilt"])
+    for col in ["GarageFinish", "BsmtQual", "FireplaceQu"]:
+        df.loc[:, col] = df.loc[:, col].fillna("None")
+    # the rest of NaNs will be filled with 0s - end model only uses numerical features
+    for col in df.columns:
+        df[col] = df[col].fillna(0)
+
+    # converting categorical values from certain features into numerical
+    for col in ["BsmtQual", "KitchenQual", "ExterQual"]:
+        df.loc[:, col] = df[col].map(generic)
+    df.loc[:, "GarageFinish"] = df["GarageFinish"].map(garage_finish)
+    df.loc[:, "FireplaceQu"] = df["FireplaceQu"].map(fireplace_quality)
+
+    # feature engineering
+    f = lambda x: 1 if x > 0 else 0
+    df["eHasPool"] = df["PoolArea"].apply(f)
+    df["eHasGarage"] = df["GarageArea"].apply(f)
+    df["eHasBsmt"] = df["TotalBsmtSF"].apply(f)
+    df["eHasFireplace"] = df["Fireplaces"].apply(f)
+    df["eHasRemodeling"] = (df["YearRemodAdd"] - df["YearBuilt"] > 0).astype(int)
+    df["eTotalSF"] = df["TotalBsmtSF"] + df["1stFlrSF"] + df["2ndFlrSF"]
+    df["eTotalBathrooms"] = (
+        df["FullBath"]
+        + (0.5 * df["HalfBath"])
+        + df["BsmtFullBath"]
+        + (0.5 * df["BsmtHalfBath"])
+    )
+    df["eOverallQual_TotalSF"] = df["OverallQual"] * df["eTotalSF"]
+
+    # final list of encoded columns
+    train_encoded_columns = [
+        "eOverallQual_TotalSF",
+        "OverallQual",
+        "eTotalSF",
+        "GrLivArea",
+        "ExterQual",
+        "KitchenQual",
+        "GarageCars",
+        "eTotalBathrooms",
+        "BsmtQual",
+        "GarageArea",
+        "TotalBsmtSF",
+        "GarageFinish",
+        "YearBuilt",
+        "eHasGarage",
+        "TotRmsAbvGrd",
+        "eHasRemodeling",
+        "FireplaceQu",
+        "MasVnrArea",
+        "eHasFireplace",
+        "eHasBsmt",
+    ]
+
+    # saving the final list of encoded columns
+    pickle.dump(train_encoded_columns, open("train_encoded_columns.pickle", "wb"))
+
+    # choosing only the final list of encoded columns
+    X_train = df[train_encoded_columns]
+
+    # standard scale data and pickle scaler
+    standard_scaler = StandardScaler()
+    X_train_ss = standard_scaler.fit_transform(X_train)
+    pickle.dump(standard_scaler, open("standard_scaler.pickle", "wb"))
+
+    # apply log to distribution of y-values
+    y_train_log = numpy.log1p(y_train)
+
+    # train and pickle model artifact
+    lasso = LassoCV(max_iter=1000)
+    lasso.fit(X_train_ss, y_train_log)
+    pickle.dump(lasso, open("lasso.pickle", "wb"))
+
+    yield
 
 
 # modelop.score
